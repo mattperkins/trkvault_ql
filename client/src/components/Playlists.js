327e3a1c56ed4321dfa9ce2345dab24e17843d43
mattperkins <github@mattperkins.com>
2018-11-21 16:55:16 +0000

    scaffold playlists query

--- client/src/components/Playlists.js
+++ client/src/components/Playlists.js
@@ -1,3 +1,26 @@
 import * as React from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
 
-export default () => <h1>Playlists</h1>
+const PLAYLISTS_QUERY = gql`
+  query PlaylistQuery {
+    playlists {
+      playlist_number
+      playlist_title
+    }
+  }
+`
+
+export default () => (
+  <Query query={PLAYLISTS_QUERY}>
+    {
+      ({ loading, error, data }) => {
+        if (loading) return <h4>Loading...</h4>
+        if (error) console.log(error)
+        console.log(data)
+
+        return <p>test</p>
+      }
+    }
+  </Query>
+)
