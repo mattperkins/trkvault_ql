0eb9cea6e9f6c3fcf05b0ef264b27ad06e798e6c
mattperkins <github@mattperkins.com>
2018-11-21 00:31:33 +0000

    refactor graphql endpoint to port 4000

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`Server running on ${PORT}`)
 )
