2da651f1d65f9cfe60ce66155b40707596f22c8c
mattperkins <github@mattperkins.com>
2018-11-22 00:16:54 +0000

    use id

--- client/src/queries/playlistQuery.js
+++ client/src/queries/playlistQuery.js
@@ -1,11 +1,20 @@
 import { gql } from 'apollo-boost'
 
-export const PLAYLISTS_QUERY = gql`
-  query PlaylistQuery {
-    playlists {
+export const PLAYLIST_QUERY = gql`
+  query PlaylistQuery($id: String!) {
+    playlist(id: $id) {
       id
       playlistNumber
       playlistTitle
+      track {
+        trackNumber
+        trackTitle
+        duration
+        genre
+        composer
+        yearRecorded
+        instrumental
+      }
     }
   }
 `
