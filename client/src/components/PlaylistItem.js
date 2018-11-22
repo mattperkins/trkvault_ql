e0b43c4199d0b74734b75f6c09e7013324f2885a
mattperkins <github@mattperkins.com>
2018-11-22 10:34:00 +0000

    add styling/classNames

--- client/src/components/PlaylistItem.js
+++ client/src/components/PlaylistItem.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 export default ({ playlistTitle, trackNumber, trackTitle, duration, genre, composer, yearRecorded, instrumental }) => {
   return (
     <div className='wrapper'>
