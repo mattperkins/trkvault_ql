d822772d972c3694cfce04ca3f8f1f6af90c03fa
mattperkins <github@mattperkins.com>
2018-11-22 00:14:19 +0000

    scaffold playlist component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Playlists from './playlists'
+import Playlist from './playlist'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
@@ -13,7 +14,10 @@ class App extends React.Component {
     return (
       <ApolloProvider client={client}>
         <Router>
-          <Route exact path='/' component={Playlists} />
+          <div className='wrapper'>
+            <Route exact path='/' component={Playlists} />
+            <Route exact path='/playlist/:id' component={Playlist} />
+          </div>
         </Router>
       </ApolloProvider>
     )
