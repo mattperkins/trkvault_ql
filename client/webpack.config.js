02c21adde5128e34ac0aeaf1b84bd6eeb7ef1ea2
mattperkins <github@mattperkins.com>
2018-11-22 20:11:45 +0000

    add output:path... and historyApiFallback to fix react-router prebundle page reload

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
   devtool: 'cheap-module-source-map',
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
+  },
+  devServer: {
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
