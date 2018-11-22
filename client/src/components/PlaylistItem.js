bcc9b4640687a6fe604b92be640b7a6903ff8a1b
mattperkins <github@mattperkins.com>
2018-11-22 00:42:28 +0000

    destructure props

--- client/src/components/PlaylistItem.js
+++ client/src/components/PlaylistItem.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default (props) => {
-  const { playlistTitle } = props
+export default ({ playlistTitle }) => {
   return (
     <div className='wrapper'>
       <div className='row'>
