355941bd82fa9fb2ff4d83a4c11c7581a453d6a2
mattperkins <github@mattperkins.com>
2018-11-22 01:38:09 +0000

    render props / display data in UI

--- client/src/components/PlaylistItem.js
+++ client/src/components/PlaylistItem.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default ({ playlistTitle }) => {
+export default ({ playlistTitle, trackNumber, trackTitle, duration, genre, composer, yearRecorded, instrumental }) => {
   return (
     <div className='wrapper'>
       <div className='row'>
 
-        <div className='col-1'>
+        <div className='col-push-2'>
           <Link to={`/`}>Home</Link>
         </div>
 
-        <div className='col-11'>
-          <p>{playlistTitle}</p>
+        <div className='col-10' style={{ marginTop: 1 }}>
+          <p>{ playlistTitle }</p>
+          <p>{ trackNumber }</p>
+          <p>{ trackTitle }</p>
+          <p>{ duration }</p>
+          <p>{ genre }</p>
+          <p>{ composer }</p>
+          <p>{ yearRecorded }</p>
+          <p>{ instrumental }</p>
         </div>
 
       </div>
